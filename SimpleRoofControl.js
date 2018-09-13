06f128e1dccd9455d63e6b279f002a656378a386
Ben L <39037350+blawson69@users.noreply.github.com>
2018-09-13 13:48:04 -0700

    Minor changes

--- SimpleRoofControl.js
+++ SimpleRoofControl.js
@@ -4,7 +4,7 @@ A roof lifting system for Roll20
 
 On Github:	https://github.com/blawson69/PurseStrings
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Buy me a coffee: https://venmo.com/theBenLawson
+Like this script? Buy me a coffee: https://venmo.com/theBenLawson
 */
 
 var SimpleRoofControl = SimpleRoofControl || (function() {
