1f0c4133af33029b309df70bafc9027f42f18002
Ben L <39037350+blawson69@users.noreply.github.com>
2019-08-30 19:24:44 -0700

    Update SimpleRoofControl.js
    Corrected Venmo link

--- SimpleRoofControl.js
+++ SimpleRoofControl.js
@@ -4,7 +4,7 @@ A roof lifting system for Roll20
 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Like this script? Buy me a coffee: https://venmo.com/theBenLawson
+Like this script? Buy me a coffee: https://venmo.com/theRealBenLawson
 */
 
 var SimpleRoofControl = SimpleRoofControl || (function () {
